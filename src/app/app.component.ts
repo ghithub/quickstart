721bdb0be691187e25d6d2d365c8e68d0d0d8a61
= <=>
2017-10-10 14:13:55 -0400

    Completed section 2 of the Heroes tutorial except I am using a template file instead of template literal.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+  id: number;
+  name: string;
+}
+
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'my-app',  
+  templateUrl: '/templates/hero.template.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  title = 'Tour of Heroes'  ;
+  hero : Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+}
