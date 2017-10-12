e172d1e10e851817f4382746dfba3ab6ebb7e92a
= <=>
2017-10-12 13:12:40 -0400

    For section 6 (Routing), added routerLink Heroes and router-outlet to this link. No issues.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<div>{{title}}</div><my-heroes></my-heroes>`
+    template: `<div>{{title}}</div>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
+    `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }        from './app.component';
+import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-import {HeroDetailComponent} from './components/heroes/hero-detail.component';
-import{HeroService} from './services/hero/hero.service';
+import { HeroDetailComponent } from './components/heroes/hero-detail.component';
+import { HeroService } from './services/hero/hero.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])],
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   bootstrap: [AppComponent],
   providers: [HeroService]
